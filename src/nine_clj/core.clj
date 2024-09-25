0c230725a36401648da93260cbac1ada8d19d3c0
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2024-09-25 21:54:22 +0200

    spawn script

--- src/nine_clj/core.clj
+++ src/nine_clj/core.clj
@@ -94,15 +94,7 @@
       level-shape (mapv phys/geom-shape level-geom)
       level-body (mapv #(phys/add-rigid-body phys-world % [0 0 0] [0 0 0] 0) level-shape)
 
-      make-char (fn [preset pos dir color side] (dat/load-char phys-world preset pos dir color side get-time))
-      step (fn [n] (* (quot (inc n) 2) (Math/pow -1 (rem n 2))))
-      step (comp (partial * 2) step)
-      make-red (fn [preset n] (make-char preset [(- 0 5 (* 2 (quot n 5))) 0 (step (mod n 10))] [1 0 0] [1 0 0 1] :red))
-      make-green (fn [preset n] (make-char preset [(+ 5 (* 2 (quot n 5))) 0 (step (mod n 10))] [-1 0 0] [0 1 0 1] :green))
-      players (concat
-        (map make-green (repeat (second presets)) (range 20))
-        (map make-red (repeat (last presets)) (range 20))
-      )
+      players ((-> "res/scripts/spawn.clj" slurp read-string eval) phys-world presets)
       player (first players)
       non-players (rest players)
     ]
