7f4fd0e7380a4a3902dcc2426277c99431c581a7
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2024-09-29 01:12:38 +0200

    license changes

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject org.clojars.taqmuraz/nine-clj "0.1.3"
   :description "Clojure OpenGL 3D graphics library"
   :url "http://github.com/Taqmuraz/nine-clj"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/MIT"}
+  :license {:name "Creative Commons Attribution-NonCommercial-NoDerivs 4.0 International"
+            :url "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode.txt"}
   :dependencies [
     [org.clojure/clojure "1.11.1"]
     [net.mikera/core.matrix "0.63.0"]
-    [io.github.taqmuraz/nine "1.2.28"]
+    [io.github.taqmuraz/nine "1.2.29"]
     [cz.advel.jbullet/jbullet "20101010-1"]
   ]
   ;:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]
