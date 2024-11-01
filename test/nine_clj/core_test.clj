d9f5d4f44e8015905c331f5a6f9d8d424fc2824f
Taqmuraz <lulook23@gmail.com>
2024-11-01 01:43:42 +0100

    clock function test

--- test/nine_clj/core_test.clj
+++ test/nine_clj/core_test.clj
@@ -3,13 +3,22 @@
     [clojure.test :refer :all]
     [nine-clj.core :refer :all]
     [nine-clj.math :as math]
+    [clojure.math :as cmath]
   )
 )
 
 (deftest clock-test
   (testing "Clock functions"
     (is (=
-      (->> 360 range (map (comp double (partial apply math/clock) math/clock-xy)))
+      (->> 360 range
+        (map (partial * 1/180 Math/PI))
+        (map math/clock-xy)
+        (map (partial apply math/clock))
+        (map double)
+        (map #(/ % 1/180 Math/PI))
+        (map cmath/round)
+        (map #(mod % 360))
+      )
       (range 360)
     ))
   )
