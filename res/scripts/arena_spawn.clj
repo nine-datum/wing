e2d4862c7ff389fbd453b6b331fd255f3c125aec
lulook <lulookgamedev@gmail.com>
2024-11-11 11:24:04 +0100

    arena character spawn time fix

--- res/scripts/arena_spawn.clj
+++ res/scripts/arena_spawn.clj
@@ -1,10 +1,9 @@
 (require
   '[nine-clj.datum :as dat]
-  '[nine-clj.core :as core]
 )
 (fn [phys-world presets]
   (let [
-      make-char (fn [preset pos dir color side] (dat/load-char phys-world preset pos dir color side :idle (core/get-time)))
+      make-char (fn [preset pos dir color side] (dat/load-char phys-world preset pos dir color side :idle 0))
       step (fn [n] (* (quot (inc n) 2) (Math/pow -1 (rem n 2))))
       step (comp (partial * 2) step)
       make-red (fn [preset n] (make-char preset [(- 0 25 (* 2 (quot n 15))) 0 (step (mod n 10))] [1 0 0] [1 0 0 1] :red))
